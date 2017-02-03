e71676b073e0a23724301749dc0a7724435a7dea
Nikolay Rudnichenko <rudnichenko.nik@gmail.com>
2017-02-03 14:13:07 +0200

    shit happens

--- app/src/main/java/com/gangrave88/testconstructor/Testing.java
+++ app/src/main/java/com/gangrave88/testconstructor/Testing.java
@@ -5,14 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.realm.Realm;
 import io.realm.RealmList;
-import io.realm.RealmResults;
 
 public class Testing extends Activity {
 
@@ -22,12 +23,13 @@ public class Testing extends Activity {
     @BindView(R.id.answer3)Button answerB3;
     @BindView(R.id.answer4)Button answerB4;
 
-    int currentId;
+    int currentQuestionId;
     String name;
     Test currentTest;
     Question currentQuestion;
     Realm realm;
 
+    ArrayList <Button> btn = new ArrayList<>();
     RealmList<Question> questions;
 
     @Override
@@ -46,22 +48,52 @@ public class Testing extends Activity {
         currentTest = realm.where(Test.class).equalTo("name",name).findFirst();
         questions = currentTest.getQuestions();
 
-        currentId = 0;
+        currentQuestionId = 0;
 
-        getNextQuestion();
+        btn.add(answerB1);
+        btn.add(answerB2);
+        btn.add(answerB3);
+        btn.add(answerB4);
+
+        getQuestion();
     }
 
-    private void getNextQuestion(){
-        if (questions.size()-1>=currentId) {
-            currentQuestion = questions.get(currentId);
+    private void getQuestion(){
+        if (currentQuestionId <= questions.size()-1) {
+            answerB1.setBackgroundResource(android.R.drawable.btn_default);
+            answerB2.setBackgroundResource(android.R.drawable.btn_default);
+            answerB3.setBackgroundResource(android.R.drawable.btn_default);
+            answerB4.setBackgroundResource(android.R.drawable.btn_default);
 
-            questionTV.setText(currentQuestion.getQuestion());
-            answerB1.setText(currentQuestion.answers.get(0).getAnswer());
-            answerB2.setText(currentQuestion.answers.get(1).getAnswer());
-            answerB3.setText(currentQuestion.answers.get(2).getAnswer());
-            answerB4.setText(currentQuestion.answers.get(3).getAnswer());
-        }
-        else endTest();
+            currentQuestion = questions.get(currentQuestionId);
+
+            if (currentQuestion.getCurrentAnswer() == null) {
+
+                questionTV.setText(currentQuestion.getQuestion());
+                answerB1.setText(currentQuestion.answers.get(0).getAnswer());
+                answerB2.setText(currentQuestion.answers.get(1).getAnswer());
+                answerB3.setText(currentQuestion.answers.get(2).getAnswer());
+                answerB4.setText(currentQuestion.answers.get(3).getAnswer());
+            } else {
+
+//                currentQuestion = questions.get(currentQuestionId);
+                for (int i = 0; i < currentQuestion.getAnswers().size(); i++) {
+                    if (currentQuestion.getAnswers().get(i) == currentQuestion.getCurrentAnswer()) {
+                        if (currentQuestion.getCurrentAnswer().isCorrect()){
+                            btn.get(i).setBackgroundResource(R.drawable.correct_answer);
+                        } else {
+                            btn.get(i).setBackgroundResource(R.drawable.uncorrect_answer);
+                        }
+                    }
+                }
+
+                questionTV.setText(currentQuestion.getQuestion());
+                answerB1.setText(currentQuestion.answers.get(0).getAnswer());
+                answerB2.setText(currentQuestion.answers.get(1).getAnswer());
+                answerB3.setText(currentQuestion.answers.get(2).getAnswer());
+                answerB4.setText(currentQuestion.answers.get(3).getAnswer());
+            }
+        } else endTest();
     }
 
     private void endTest(){
@@ -90,18 +122,20 @@ public class Testing extends Activity {
                 break;
             }
         }
-        currentId++;
-        getNextQuestion();
+
+        currentQuestionId++;
+        getQuestion();
+
     }
 
-    private void answer(int id,Button btn){
+    private void answer(int id, Button btn){
         Answer currentAnswer = currentQuestion.getAnswers().get(id);
         if (currentAnswer.isCorrect()){
             btn.setBackgroundResource(R.drawable.correct_answer);
-        }
-        else{
+        } else{
             btn.setBackgroundResource(R.drawable.uncorrect_answer);
         }
+
         realm.beginTransaction();
         currentQuestion.setCurrentAnswer(currentAnswer);
         realm.commitTransaction();
@@ -109,13 +143,17 @@ public class Testing extends Activity {
 
     @OnClick(R.id.test_previous_question)
     public void previousQuestion(){
-        currentId--;
-        getNextQuestion();
+        if (currentQuestionId > 0){
+            currentQuestionId--;
+            getQuestion();
+        }
     }
 
     @OnClick(R.id.test_next_question)
     public void nextQuestion(){
-        currentId++;
-        getNextQuestion();
+        if (currentQuestionId < currentTest.getQuestionCount()-1){
+            currentQuestionId++;
+            getQuestion();
+        }
     }
 }
